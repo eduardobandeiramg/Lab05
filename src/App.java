2a49e83f9f74afd4b51d0bec85339e9bdb64141c
eduardobandeiramg <eduardobandeiramg@gmail.com>
2023-09-03 19:23:40 -0300

    primeiro

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+
+    public static void funcaoPrinta(String nome , String disciplina){
+        System.out.println("Aluno: \n" + nome + "\nDisciplina cursada: \n" + disciplina);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        funcaoPrinta("Eduardo" , "Engenharia Economica para Software");
+        }
+}
+
