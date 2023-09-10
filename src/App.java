0a5cb40721b3e9ba814554f0cb6e5673f27703a2
eduardobandeiramg <eduardobandeiramg@gmail.com>
2023-09-10 14:22:39 -0300

    abc

--- src/App.java
+++ src/App.java
@@ -1,7 +1,18 @@
 public class App {
-
-    public static void funcaoPrinta(String nome , String disciplina){
-        System.out.println("Aluno: \n" + nome + "\nDisciplina cursada: \n" + disciplina);
+    public static void funcaoPrinta(String nome , String disciplina)
+    {
+        System.out.println("Aluno: \n"
+        + nome 
+        + "\nDisciplina cursada: \n" 
+        + disciplina);
+        //Adicionando uma linha de comentário
+        //Adicionando outra linha de comentário
+        //Mudanças realizadas:
+        //primeira chave para a linha de baixo
+        //adicionadas mais 3 linhas para a função print (cada elemento a ser printado em uma linha diferente).
+        /* Comentário de múltiplas linhas para quantidade de novas linhas esperadas:
+        * 12 linhas a mais são esperadas, indo para 15.
+        * */
     }
 
     public static void main(String[] args) throws Exception {
