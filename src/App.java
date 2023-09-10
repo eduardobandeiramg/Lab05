93b66afbe4012dfdc8325ec71ddd30591a474638
eduardobandeiramg <eduardobandeiramg@gmail.com>
2023-09-10 14:27:43 -0300

    aumentando as linhas do metodo

--- src/App.java
+++ src/App.java
@@ -5,10 +5,10 @@ public class App {
         + nome 
         + "\nDisciplina cursada: \n" 
         + disciplina);
-        //Adicionando uma linha de comentário
-        //Adicionando outra linha de comentário
+        //Adicionando uma linha de comentário.
+        //Adicionando outra linha de comentário.
         //Mudanças realizadas:
-        //primeira chave para a linha de baixo
+        //primeira chave para a linha de baixo.
         //adicionadas mais 3 linhas para a função print (cada elemento a ser printado em uma linha diferente).
         /* Comentário de múltiplas linhas para quantidade de novas linhas esperadas:
         * 12 linhas a mais são esperadas, indo para 15.
